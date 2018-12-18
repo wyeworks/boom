6646ea78aaea236f68e2865a2c98c45b78941452
Jorge Bejar <jorge@wyeworks.com>
2018-12-18 20:18:56 -0300

    Rescue internal errors

--- lib/boom.ex
+++ lib/boom.ex
@@ -6,20 +6,22 @@ defmodule Boom do
       import Bamboo.Email
 
       defp handle_errors(conn, %{reason: exception}) do
-        IO.inspect exception, label: "Boom library output hackaton"
+        try do
+          welcome_email = new_email
+            |> to("foo@example.com")
+            |> from("me@example.com")
+            |> subject("Welcome!!!")
+            |> html_body("<strong>Welcome</strong>")
+            |> text_body("welcome")
 
-        welcome_email = new_email
-          |> to("foo@example.com")
-          |> from("me@example.com")
-          |> subject("Welcome!!!")
-          |> html_body("<strong>Welcome</strong>")
-          |> text_body("welcome")
-
-        welcome_email |> mailer.deliver_now
+          welcome_email |> mailer.deliver_now
+        rescue
+          e -> IO.inspect(e, label: "[Boom] Error sending exception")
+        end
       end
 
       defp mailer do
-        Application.get_env(:boom, :mailer)
+        Application.fetch_env!(:boom, :mailer)
       end
     end
   end
