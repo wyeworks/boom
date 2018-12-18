8774afe6a437a1f09440b85883ed0e3373329532
Leticia Errandonea <errandonealeticia@gmail.com>
2018-12-18 20:54:40 -0300

    Fix library warnings

--- lib/boom/mailer_helper.ex
+++ lib/boom/mailer_helper.ex
@@ -1,16 +1,16 @@
 defmodule Boom.MailerHelper do
   import Bamboo.Email
 
-  def notify(exception) do
+  def notify(_) do
     welcome_email =
-      new_email
+      new_email()
       |> to("foo@example.com")
       |> from("me@example.com")
       |> subject("Welcome!!!")
       |> html_body("<strong>Welcome</strong>")
       |> text_body("welcome")
 
-    welcome_email |> mailer.deliver_now
+    welcome_email |> mailer().deliver_now
   end
 
   defp mailer do
