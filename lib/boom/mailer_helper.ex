604beb0e7fca5fe68d8af6867aa1e2f8c5bf9b98
Jorge Bejar <jorge@wyeworks.com>
2018-12-18 20:27:31 -0300

    Minor changes suggested by format

---
+++ lib/boom/mailer_helper.ex
@@ -0,0 +1,19 @@
+defmodule Boom.MailerHelper do
+  import Bamboo.Email
+
+  def notify(exception) do
+    welcome_email =
+      new_email
+      |> to("foo@example.com")
+      |> from("me@example.com")
+      |> subject("Welcome!!!")
+      |> html_body("<strong>Welcome</strong>")
+      |> text_body("welcome")
+
+    welcome_email |> mailer.deliver_now
+  end
+
+  defp mailer do
+    Application.fetch_env!(:boom, :mailer)
+  end
+end
