559a67ee41aca403fabc527e9b6dc8b97030aa8c
Jorge Bejar <jorge@wyeworks.com>
2018-12-18 20:26:34 -0300

    Rescue internal errors

--- lib/boom.ex
+++ lib/boom.ex
@@ -3,25 +3,14 @@ defmodule Boom do
     quote location: :keep do
       use Plug.ErrorHandler
       import Boom
-      import Bamboo.Email
+      alias Boom.MailerHelper
 
       defp handle_errors(conn, %{reason: exception}) do
         try do
-          welcome_email = new_email
-            |> to("foo@example.com")
-            |> from("me@example.com")
-            |> subject("Welcome!!!")
-            |> html_body("<strong>Welcome</strong>")
-            |> text_body("welcome")
-
-          welcome_email |> mailer.deliver_now
+          MailerHelper.notify(exception)
         rescue
           e -> IO.inspect(e, label: "[Boom] Error sending exception")
         end
-      end
-
-      defp mailer do
-        Application.fetch_env!(:boom, :mailer)
       end
     end
   end
