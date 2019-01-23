473a7a67fdff065fb2cf2cb74220f9312a0511fd
Jorge Bejar <jorge@wyeworks.com>
2019-01-23 20:33:14 -0300

    Add more basic mailer tests

--- test/mailer_test.exs
+++ test/mailer_test.exs
@@ -7,6 +7,9 @@ defmodule MailerTest do
   defmodule FakeMailer do
     def deliver_now(email) do
       send self(), {:email_subject, email.subject}
+      send self(), {:email_from, email.from}
+      send self(), {:email_to, email.to}
+      send self(), {:email_text_body, email.text_body}
     end
   end
 
@@ -35,5 +38,29 @@ defmodule MailerTest do
     catch_error TestPlug.call(conn, [])
 
     assert_received {:email_subject, "BOOM error caught: booom!"}
+  end
+
+  test "Set email using proper from and to addresses" do
+    conn = conn(:get, "/")
+    catch_error TestPlug.call(conn, [])
+
+    assert_received {:email_from, "me@example.com"}
+    assert_received {:email_to, "foo@example.com"}
+  end
+
+  test "Set email text body with exception stacktrace" do
+    conn = conn(:get, "/")
+    catch_error TestPlug.call(conn, [])
+
+    expection_first_line = "test/mailer_test.exs:24: MailerTest.TestPlug.\"call (overridable 1)\"/2\n"
+    assert_received {:email_text_body, [ expection_first_line | _ ]}
+  end
+
+  test "Set email text body with exception stacktrace" do
+    conn = conn(:get, "/")
+    catch_error TestPlug.call(conn, [])
+
+    expection_first_line = "test/mailer_test.exs:24: MailerTest.TestPlug.\"call (overridable 1)\"/2\n"
+    assert_received {:email_text_body, [ expection_first_line | _ ]}
   end
 end
