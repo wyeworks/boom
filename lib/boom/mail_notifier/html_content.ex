6cc8e3d46876b276aefa30fa3511c66c0e991598
Jorge Bejar <jorge@wyeworks.com>
2019-05-05 23:25:21 -0300

    Use template for email HTML

--- lib/boom/mail_notifier/html_content.ex
+++ lib/boom/mail_notifier/html_content.ex
@@ -1,10 +1,6 @@
 defmodule Boom.MailNotifier.HTMLContent do
   import Boom.MailNotifier.Helpers
 
-  def build(%ErrorInfo{controller: nil, stack: stack}) do
-    stack_to_html(stack)
-  end
-
   def build(%ErrorInfo{
         name: name,
         controller: controller,
@@ -12,45 +8,32 @@ defmodule Boom.MailNotifier.HTMLContent do
         request: request,
         stack: stack
       }) do
-    "<p>#{exception_basic_text(name, controller, action)}</p>" <>
-      request_info_to_html(request) <>
-      stack_to_html(stack)
+    exception_summary =
+      if controller && action do
+        exception_basic_text(name, controller, action)
+      end
+
+    EEx.eval_file(template_path(),
+      exception_summary: exception_summary,
+      request: request,
+      exception_stack_entries: Enum.map(stack, &entry_to_map/1)
+    )
   end
 
-  defp request_info_to_html(request) do
-    [
-      "Request Information:",
-      "URL: #{request.url}",
-      "Path: #{request.path}",
-      "Method: #{request.method}",
-      "Port: #{request.port}",
-      "Scheme: #{request.scheme}",
-      "Query String: #{request.query_string}",
-      "Client IP: #{request.client_ip}"
-    ]
-    |> Enum.map(&wrap_html_item/1)
-    |> wrap_html_list
+  defp template_path do
+    current_folder_path = Path.dirname(__ENV__.file)
+    Path.join([current_folder_path, "templates", "email_body.html.eex"])
   end
 
-  defp stack_to_html(stack) do
-    stack
-    |> Enum.map(&stack_entry_to_html/1)
-    |> wrap_html_list
-  end
-
-  defp stack_entry_to_html(entry) do
+  defp entry_to_map(entry) do
     {module, function, arity, [file: file, line: line]} = entry
-    left = "<span>#{file}:#{line}</span>"
-    right = "<span style=\"float: right\">#{module}.#{function}/#{arity}</span>"
 
-    wrap_html_item(left <> right)
-  end
-
-  defp wrap_html_item(html) do
-    "<li>#{html}</li>"
-  end
-
-  defp wrap_html_list(html) do
-    "<ul style=\"list-style-type: none;\">#{html}</ul>"
+    %{
+      module: module,
+      function: function,
+      arity: arity,
+      file: file,
+      line: line
+    }
   end
 end
