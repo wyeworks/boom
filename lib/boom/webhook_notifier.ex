d2065e2855ac936b520ed1f207680c4683a3b589
Ignacio Aguirrezabal <ignacio@wyeworks.com>
2019-08-29 21:05:06 -0300

    Send error info json to the provided webhook

---
+++ lib/boom/webhook_notifier.ex
@@ -0,0 +1,35 @@
+defmodule Boom.WebhookNotifier do
+  @behaviour Boom.Notifier
+
+  use HTTPoison.Base
+
+  import Boom.MailNotifier.Helpers
+
+  @impl Boom.Notifier
+  def notify(error_info, url: url) do
+    post!(url, error_info, [{"Content-Type", "application/json"}])
+  end
+
+  @impl true
+  def process_request_body(body), do: json(body)
+
+  defp json(%ErrorInfo{
+         name: name,
+         controller: controller,
+         action: action,
+         request: request,
+         stack: stack
+       }) do
+    exception_summary =
+      if controller && action do
+        exception_basic_text(name, controller, action)
+      end
+
+    %{
+      exception_summary: exception_summary,
+      request: request,
+      exception_stack_entries: Enum.map(stack, &Exception.format_stacktrace_entry/1)
+    }
+    |> Poison.encode!()
+  end
+end
