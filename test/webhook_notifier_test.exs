71b132372b2dc03b01b96ab701128fa1c1e3afa5
Ignacio Aguirrezabal <ignacio@wyeworks.com>
2020-09-03 18:33:01 -0300

    Run test in Travis

--- test/webhook_notifier_test.exs
+++ test/webhook_notifier_test.exs
@@ -4,33 +4,22 @@ defmodule WebhookNotifierTest do
 
   doctest Boom
 
-  @expected_response [
-                       %{
-                         exception_summary:
-                           "TestException occurred while the request was processed by TestController#index",
-                         exception_stack_entries: [
-                           "test/webhook_notifier_test.exs:44: WebhookNotifierTest.TestController.index/2",
-                           "test/webhook_notifier_test.exs:35: WebhookNotifierTest.TestController.action/2",
-                           "test/webhook_notifier_test.exs:35: WebhookNotifierTest.TestController.phoenix_controller_pipeline/2",
-                           "(phoenix) lib/phoenix/router.ex:275: Phoenix.Router.__call__/1",
-                           "lib/plug/error_handler.ex:64: WebhookNotifierTest.TestRouter.call/2",
-                           "test/webhook_notifier_test.exs:80: WebhookNotifierTest.\"test request is sent to webhook\"/1",
-                           "(ex_unit) lib/ex_unit/runner.ex:355: ExUnit.Runner.exec_test/1",
-                           "(stdlib) timer.erl:166: :timer.tc/1",
-                           "(ex_unit) lib/ex_unit/runner.ex:306: anonymous fn/4 in ExUnit.Runner.spawn_test_monitor/4"
-                         ],
-                         request: %{
-                           client_ip: "127.0.0.1",
-                           method: "GET",
-                           path: "/",
-                           port: 80,
-                           query_string: "",
-                           scheme: "http",
-                           url: "http://www.example.com/"
-                         }
-                       }
-                     ]
-                     |> Jason.encode!()
+  @expected_response %{
+    exception_summary:
+      "TestException occurred while the request was processed by TestController#index",
+    exception_stack_entries: [
+      "test/webhook_notifier_test.exs:44: WebhookNotifierTest.TestController.index/2"
+    ],
+    request: %{
+      client_ip: "127.0.0.1",
+      method: "GET",
+      path: "/",
+      port: 80,
+      query_string: "",
+      scheme: "http",
+      url: "http://www.example.com/"
+    }
+  }
 
   defmodule TestController do
     use Phoenix.Controller
@@ -72,7 +61,22 @@ defmodule WebhookNotifierTest do
     Bypass.expect(bypass, fn conn ->
       assert "POST" == conn.method
       {:ok, body, _conn} = Plug.Conn.read_body(conn)
-      assert body == @expected_response
+
+      [
+        %{
+          exception_summary: exception_summary,
+          exception_stack_entries: [first_stack_entry | _] = exception_stack_entries,
+          request: request
+        }
+      ] = Jason.decode!(body, keys: :atoms)
+
+      assert exception_summary == @expected_response.exception_summary
+
+      assert length(exception_stack_entries) == 9
+      assert first_stack_entry =~ "WebhookNotifierTest.TestController.index/2"
+
+      assert request == @expected_response.request
+
       Plug.Conn.resp(conn, 200, [])
     end)
 
