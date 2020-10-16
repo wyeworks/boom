f8d813c85606e4b7b1fe82743a56392521b23948
sevgit <sebastian.ventura@wyeworks.com>
2020-10-16 17:01:41 -0300

    Raise on configuration issues

--- lib/boom.ex
+++ lib/boom.ex
@@ -4,48 +4,65 @@ defmodule Boom do
 
   alias Boom.ErrorStorage
 
+  defmodule ConfigException do
+    defexception message: "Boom configuration error encountered"
+  end
+
   defmacro __using__(config) do
-    quote location: :keep do
-      use Plug.ErrorHandler
 
-      import Boom
+    with :ok <- validate_config(config) do    
+      quote location: :keep do
+        use Plug.ErrorHandler
 
-      def handle_errors(conn, error) do
-        {error_kind, error_info} = ErrorInfo.build(error, conn)
+        import Boom
 
-        ErrorStorage.add_errors(error_kind, error_info)
+        def handle_errors(conn, error) do
+          {error_kind, error_info} = ErrorInfo.build(error, conn)
 
-        if ErrorStorage.send_notification?(error_kind) do
-          occurrences = ErrorStorage.get_errors(error_kind)
+          ErrorStorage.add_errors(error_kind, error_info)
 
-          settings = unquote(config)
+          if ErrorStorage.send_notification?(error_kind) do
+            occurrences = ErrorStorage.get_errors(error_kind)
 
-          case Keyword.get(settings, :notifiers) do
-            nil ->
-              with {:ok, notifier} <- Keyword.fetch(settings, :notifier),
-                   {:ok, options} <- Keyword.fetch(settings, :options) do
-                notifier.notify(occurrences, options)
-              end
+            settings = unquote(config)
 
-            notifiers_config when is_list(notifiers_config) ->
-              for notifier_config <- notifiers_config do
-                with {:ok, notifier} <- Keyword.fetch(notifier_config, :notifier),
-                     {:ok, options} <- Keyword.fetch(notifier_config, :options) do
+            case Keyword.get(settings, :notifiers) do
+              nil ->
+                with {:ok, notifier} <- Keyword.fetch(settings, :notifier),
+                    {:ok, options} <- Keyword.fetch(settings, :options) do
                   notifier.notify(occurrences, options)
                 end
-              end
+
+              notifiers_config when is_list(notifiers_config) ->
+                for notifier_config <- notifiers_config do
+                  with {:ok, notifier} <- Keyword.fetch(notifier_config, :notifier),
+                      {:ok, options} <- Keyword.fetch(notifier_config, :options) do
+                    notifier.notify(occurrences, options)
+                  end
+                end
+            end
+
+            {notification_trigger, _settings} =
+              Keyword.pop(settings, :notification_trigger, :always)
+
+            ErrorStorage.clear_errors(notification_trigger, error_kind)
           end
-
-          {notification_trigger, _settings} =
-            Keyword.pop(settings, :notification_trigger, :always)
-
-          ErrorStorage.clear_errors(notification_trigger, error_kind)
+        rescue
+          # FIXME: we should handle this in a different way
+          # credo:disable-for-next-line
+          e -> IO.inspect(e, label: "[Boom] Error sending exception")
         end
-      rescue
-        # FIXME: we should handle this in a different way
-        # credo:disable-for-next-line
-        e -> IO.inspect(e, label: "[Boom] Error sending exception")
       end
+  end
+end
+
+  defp validate_config(config) do
+    with  {:validate_notifier, {:ok, _notifier}} <- {:validate_notifier, Keyword.fetch(config, :notifier)},
+          {:validate_options, {:ok, _options}} <- {:validate_options, Keyword.fetch(config, :options)} do
+      :ok
+    else 
+      {:validate_notifier, _} -> raise ConfigException, message: "No notifier found"
+      {:validate_options, _} -> raise ConfigException, message: "No options map found"
     end
   end
 end
