814a5b5566639c021668700de5056062b2e37a47
Ignacio Aguirrezabal <ignacio@wyeworks.com>
2021-08-25 16:25:13 -0300

    Refactor error storage data
    Currently, we are grouping every error information in several lists
    according to its error type. Eventually, this could consume a lot of
    memory without providing any important benefit (the only piece of
    information that changes is the timestamp and the request data).
    
    This commit replaces that with a map structure where its key is the
    hashed error info and its value is the actual error info plus:
      * the number of times this error occurred,
      * the first and the list time it happened.

--- lib/boom_notifier/error_storage.ex
+++ lib/boom_notifier/error_storage.ex
@@ -4,6 +4,8 @@ defmodule BoomNotifier.ErrorStorage do
   # Keeps track of the errors grouped by type and a counter so the notifier
   # knows the next time it should be executed
 
+  defstruct [:accumulated_occurrences, :first_occurrence, :last_occurrence]
+
   use Agent, start: {__MODULE__, :start_link, []}
 
   @spec start_link() :: Agent.on_start()
@@ -11,57 +13,114 @@ defmodule BoomNotifier.ErrorStorage do
     Agent.start_link(fn -> %{} end, name: :boom_notifier)
   end
 
-  @spec add_errors(atom(), %ErrorInfo{}) :: :ok
-  def add_errors(error_kind, error_info) do
+  @spec add_error(%ErrorInfo{}) :: :ok
+  def add_error(error_info) do
+    error_hash_key = generate_error_key(error_info)
+    timestamp = DateTime.utc_now()
+
     Agent.update(
       :boom_notifier,
-      &Map.update(&1, error_kind, {1, [error_info]}, fn {counter, errors} ->
-        {counter, [error_info | errors]}
-      end)
+      &Map.update(
+        &1,
+        error_hash_key,
+        %{
+          accumulated_occurrences: 1,
+          max_storage_capacity: 1,
+          first_occurrence: timestamp,
+          last_occurrence: timestamp
+        },
+        fn error_storage_item ->
+          error_storage_item
+          |> Map.update!(:accumulated_occurrences, fn current -> current + 1 end)
+          |> Map.update!(:first_occurrence, fn first_occurrence ->
+            first_occurrence || timestamp
+          end)
+          |> Map.update!(:last_occurrence, fn _ -> timestamp end)
+        end
+      )
     )
   end
 
-  @spec get_errors(atom()) :: list(%ErrorInfo{})
-  def get_errors(error_kind) do
+  @spec get_error_storage_item(%ErrorInfo{}) :: %__MODULE__{}
+  def get_error_storage_item(error_info) do
+    error_hash_key = generate_error_key(error_info)
+
     Agent.get(:boom_notifier, fn state -> state end)
-    |> Map.get(error_kind)
-    |> case do
-      nil -> nil
-      {_counter, errors} -> errors
-    end
+    |> Map.get(error_hash_key)
   end
 
-  @spec send_notification?(atom()) :: boolean()
-  def send_notification?(error_kind) do
-    Agent.get(:boom_notifier, fn state -> state end)
-    |> Map.get(error_kind)
-    |> case do
-      nil -> false
-      {counter, errors} -> length(errors) >= counter
-    end
+  @spec send_notification?(%ErrorInfo{}) :: boolean()
+  def send_notification?(error_info) do
+    error_hash_key = generate_error_key(error_info)
+
+    error_storage_item =
+      Agent.get(:boom_notifier, fn state -> state end)
+      |> Map.get(error_hash_key)
+
+    accumulated_occurrences = Map.get(error_storage_item, :accumulated_occurrences)
+    max_storage_capacity = Map.get(error_storage_item, :max_storage_capacity)
+
+    if accumulated_occurrences >= max_storage_capacity, do: true, else: false
   end
 
   @type error_strategy :: :always | :exponential | [exponential: [limit: non_neg_integer()]]
 
-  @spec clear_errors(error_strategy, atom()) :: :ok
-  def clear_errors(:exponential, error_kind) do
+  @spec reset_accumulated_errors(error_strategy, %ErrorInfo{}) :: :ok
+  def reset_accumulated_errors(:exponential, error_info) do
+    error_hash_key = generate_error_key(error_info)
+
     Agent.update(
       :boom_notifier,
-      &Map.update!(&1, error_kind, fn {counter, _errors} -> {counter * 2, []} end)
+      &Map.update!(&1, error_hash_key, fn error_storage_item ->
+        clear_values(error_storage_item)
+        |> Map.update!(:max_storage_capacity, fn current -> current * 2 end)
+      end)
     )
   end
 
-  def clear_errors([exponential: [limit: limit]], error_kind) do
+  def reset_accumulated_errors([exponential: [limit: limit]], error_info) do
+    error_hash_key = generate_error_key(error_info)
+
     Agent.update(
       :boom_notifier,
-      &Map.update!(&1, error_kind, fn {counter, _errors} -> {min(counter * 2, limit), []} end)
+      &Map.update!(&1, error_hash_key, fn error_storage_item ->
+        clear_values(error_storage_item)
+        |> Map.update!(:max_storage_capacity, fn current -> min(current * 2, limit) end)
+      end)
     )
   end
 
-  def clear_errors(:always, error_kind) do
+  def reset_accumulated_errors(:always, error_info) do
+    error_hash_key = generate_error_key(error_info)
+
     Agent.update(
       :boom_notifier,
-      &Map.update!(&1, error_kind, fn _value -> {1, []} end)
+      &Map.update!(&1, error_hash_key, fn error_storage_item ->
+        clear_values(error_storage_item)
+        |> Map.replace!(:max_storage_capacity, 1)
+      end)
     )
+  end
+
+  # Generates a unique hash key based on the error info. The timestamp and the
+  # request info is removed so we don't get different keys for the same error.
+  #
+  # The map is converted to a string using `inspect()` so we can hash it using
+  # the crc32 algorithm that was taken from the Exception Notification library
+  # for Rails
+  @spec generate_error_key(%ErrorInfo{}) :: non_neg_integer()
+  defp generate_error_key(error_info) do
+    error_info
+    |> Map.delete(:request)
+    |> Map.delete(:timestamp)
+    |> inspect()
+    |> :erlang.crc32()
+  end
+
+  defp clear_values(error_storage_item) do
+    error_storage_item
+    |> Map.replace!(:accumulated_occurrences, 0)
+    |> Map.replace!(:first_occurrence, nil)
+    |> Map.replace!(:last_occurrence, nil)
   end
 end
