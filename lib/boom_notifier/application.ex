8def4b7fb4184e52765bb55c27a4d721a44154f1
Oliver Marriott <hello@omarriott.com>
2021-10-09 06:41:14 +1100

    Add AsyncEmailSupervisor to application children

--- lib/boom_notifier/application.ex
+++ lib/boom_notifier/application.ex
@@ -6,7 +6,8 @@ defmodule BoomNotifier.Application do
   def start(_type, _args) do
     children = [
       BoomNotifier.ErrorStorage,
-      BoomNotifier.NotifierSenderServer
+      BoomNotifier.NotifierSenderServer,
+      {Task.Supervisor, name: BoomNotifier.AsyncEmailSupervisor}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one)
