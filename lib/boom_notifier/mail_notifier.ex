0e89e4a1b853cce73fed260d4cfef101bd577512
Oliver Marriott <hello@omarriott.com>
2021-10-09 06:41:37 +1100

    Update MailNotifier to use Swoosh instead of Bamboo

--- lib/boom_notifier/mail_notifier.ex
+++ lib/boom_notifier/mail_notifier.ex
@@ -1,6 +1,6 @@
 defmodule BoomNotifier.MailNotifier do
   @moduledoc """
-  Send exception notification by email using `Bamboo`.
+  Send exception notification by email using `Swoosh`.
 
   ## Usage
   ```elixir
@@ -22,7 +22,7 @@ defmodule BoomNotifier.MailNotifier do
 
   @behaviour BoomNotifier.Notifier
 
-  import Bamboo.Email
+  import Swoosh.Email
 
   alias BoomNotifier.MailNotifier.HTMLContent
   alias BoomNotifier.MailNotifier.TextContent
@@ -45,14 +45,26 @@ defmodule BoomNotifier.MailNotifier do
   @impl BoomNotifier.Notifier
   @spec notify(list(%ErrorInfo{}), options) :: no_return()
   def notify(error_info, options) do
+    # Note, unlike Bamboo, Swoosh will raise while creating the mail if it is
+    # invalid (has a bad recipient, etc). To consumers of this function, the
+    # behaviour is identical to Bamboo's `deliver_later!`.
     email =
-      new_email()
+      new()
       |> to(options[:to])
       |> from(options[:from])
       |> subject("#{options[:subject]}: #{hd(error_info) |> Map.get(:reason)}")
       |> html_body(HTMLContent.build(error_info))
       |> text_body(TextContent.build(error_info))
 
-    options[:mailer].deliver_later!(email)
+    # Swoosh does not provide async send, but it recommends using Elixir tasks.
+    # This is actually identical to Bamboo, Bamboo just wraps the boiler plate.
+    # This could also just be Task.start since Boom doesn't seem to care about
+    # tracking delivery results?
+    Task.Supervisor.start_child(BoomNotifier.AsyncEmailSupervisor, fn ->
+      options[:mailer].deliver!(email)
+    end)
+
+    # return email to mirror bamboo deliver_later!
+    email
   end
 end
