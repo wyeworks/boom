379890d2b0c37c96e87d8960455d19a724cbd05f
Gonzalo <grzuy0@gmail.com>
2021-10-15 20:09:04 -0300

    WIP Attempt to improve legibility of stacktrace
    Co-authored-by: lucianoruizmorales@gmail.com

--- lib/boom_notifier/mail_notifier/html_content.ex
+++ lib/boom_notifier/mail_notifier/html_content.ex
@@ -32,10 +32,32 @@ defmodule BoomNotifier.MailNotifier.HTMLContent do
     %{
       exception_summary: exception_summary,
       request: request,
-      exception_stack_entries: Enum.map(stack, &Exception.format_stacktrace_entry/1),
+      exception_stack_entries: Enum.map(stack, &normalize_entry/1),
       timestamp: format_timestamp(timestamp),
       metadata: metadata
     }
+  end
+
+  defp normalize_entry({module, fun, arity, location}) do
+    {format_application(module), module, fun, arity, location}
+  end
+
+  # TODO: Not use copied Exception.format_application/1
+  defp format_application(module) do
+    # We cannot use Application due to bootstrap issues
+    case :application.get_application(module) do
+      {:ok, app} ->
+        case :application.get_key(app, :vsn) do
+          {:ok, vsn} when is_list(vsn) ->
+            "(" <> Atom.to_string(app) <> " " <> List.to_string(vsn) <> ") "
+
+          _ ->
+            "(" <> Atom.to_string(app) <> ") "
+        end
+
+      :undefined ->
+        ""
+    end
   end
 
   defp format_timestamp(timestamp) do
