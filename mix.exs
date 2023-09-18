abac2651c0bb4e40ff7e93e53050443580512f54
Ignacio Aguirrezabal <ignacio@wyeworks.com>
2023-09-18 10:43:22 -0300

    Remove support for elixir 1.8 (#87)

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule BoomNotifier.MixProject do
     [
       app: :boom_notifier,
       version: "0.8.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
