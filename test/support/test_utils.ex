14404908ea7aeda7692a8fd182c98340d41bf887
Andrés Vidal <andres@wyeworks.com>
2024-07-12 13:10:23 -0300

    Use built-in Version.compare in above_version?
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- test/support/test_utils.ex
+++ test/support/test_utils.ex
@@ -4,15 +4,6 @@ defmodule TestUtils do
   """
 
   def above_version?(boundary) do
-    [major, minor, patch] = split_version(boundary)
-    [system_major, system_minor, system_patch] = split_version(System.version())
-
-    system_major > major or
-      (system_major == major and system_minor > minor) or
-      (system_major == major and system_minor == minor and system_patch > patch)
-  end
-
-  defp split_version(version) when is_binary(version) do
-    version |> String.split(".") |> Enum.map(&String.to_integer/1)
+    Version.compare(System.version(), boundary) == :gt
   end
 end
